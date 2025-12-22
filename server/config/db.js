c4b1f5d7ef5808a5d807edb0a2d65e843dbfbf47
Rafat Haroub <ra2fethroub@gmail.com>
2025-12-22 13:26:35 +0300

    fix: Correct typo in database host comment.

--- server/config/db.js
+++ server/config/db.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 require('dotenv').config();
 
 const db = mysql.createConnection({
-  host: process.env.DB_HOST || 'localhost', //rafattewst
+  host: process.env.DB_HOST || 'localhost', //rafattest
   user: process.env.DB_USER || 'root',
   password: process.env.DB_PASSWORD || '',
   database: process.env.DB_NAME || 'your_database',
