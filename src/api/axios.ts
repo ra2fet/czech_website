c3b0afac3c6c75d06e96a152abe4a21bd9851496
Rafat Haroub <ra2fethroub@gmail.com>
2025-12-22 09:41:56 +0300

    test deploy

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -6,7 +6,7 @@ interface ApiError {
 }
 
 const instance = axios.create({
-  // baseURL: 'http://localhost:5001/api',  local api aa
+  // baseURL: 'http://localhost:5001/api',  local api aaa
   baseURL: 'https://babobambo.com/api/v2',
 });
 
