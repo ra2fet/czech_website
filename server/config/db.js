9c2ee4fd5a5c30b7c7810c3f9b00eea802339cc9
Rafat Haroub <ra2fethroub@gmail.com>
2025-12-22 13:42:37 +0300

    test dep

--- server/config/db.js
+++ server/config/db.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 require('dotenv').config();
 
 const db = mysql.createConnection({
-  host: process.env.DB_HOST || 'localhost', //rafattesttt
+  host: process.env.DB_HOST || 'localhost', //rafattestttt
   user: process.env.DB_USER || 'root',
   password: process.env.DB_PASSWORD || '',
   database: process.env.DB_NAME || 'your_database',
